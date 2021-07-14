eefc3d4562bd0bb99b28d0348b252c7d96ebd643
sinacodes95 <sina.kabki@bbc.co.uk>
2021-07-14 13:28:05 +0100

    Add eslint to project
    eslint will allow for standardisation of our code and provide consistency throughout the project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      es6: true,
+      node: true,
+      jasmine: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:jest/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2019,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    rules: {
+      "complexity": ["error", { max: 10 }],
+      "@typescript-eslint/no-floating-promises": ["error"],
+      "@typescript-eslint/prefer-for-of": ["error"],
+      "@typescript-eslint/no-unnecessary-condition": ["error", { "allowConstantLoopConditions": true }],
+    }
+  }
