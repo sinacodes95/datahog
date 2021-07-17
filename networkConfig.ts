d15ce258a0d898bc040464299721c9820a98b148
sinacodes95 <sina.kabki@bbc.co.uk>
2021-07-17 10:44:34 +0100

    Update networkConfig and deps

--- networkConfig.ts
+++ networkConfig.ts
@@ -1,3 +1,3 @@
-export const PROVIDERS_BASE_URL = process.env.PROVIDERS_BASE_URL || 'http://localhost:3000';
+export const PROVIDERS_BASE_URL = process.env.PROVIDERS_BASE_URL || 'providers-api://providers-api:3000';
 export const WEBHOOKS_BASE_URL = process.env.WEBHOOKS_BASE_URL || 'http://localhost:4000';
-export const CALLBACK_BASE_URL = process.env.CALLBACK_BASE_URL || 'http://localhost:5000';
+export const CALLBACK_BASE_URL = process.env.CALLBACK_BASE_URL || 'callback-api://callback-api:5000';
